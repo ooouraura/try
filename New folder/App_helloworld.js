e89ecade9206fe6c0488c16c9e80a9e9b48fcc62
Aura <76034378+ooouraura@users.noreply.github.com>
2021-11-07 15:43:48 +0700

    Add files via upload

---
+++ New folder/App_helloworld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const HelloWorldApp = () => {
+    return (
+    <View>
+        <Text>Hello world!</Text>
+        </View>
+        )
+    }
+    export default HelloWorldApp;
